6dcc4a1214270c24c2775aa21f472a4da70ff077
craig doyle <32498304+Das-Horn@users.noreply.github.com>
2023-09-04 11:48:18 +0100

    Ads push

--- pages/index.js
+++ pages/index.js
@@ -22,6 +22,7 @@ export default function Home() {
     `}>
       <Head>
         <title>QR Code Generator</title>
+        <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3041374135994224" crossorigin="anonymous"></script>
       </Head>
       <Header />
       <Content>
